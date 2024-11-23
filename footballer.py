3b9a5d30bc4e40741ac943ce99fb980e25c031ec
1r9m9c6 <roberto.m.colombo@gmail.com>
2024-11-23 18:01:34 +0100

    many improvements, TODO: continue with playuer

--- footballer.py
+++ footballer.py
@@ -1,5 +1,16 @@
+import logging
 from utility import calc_factorial
 import random
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
 
 class Footballer:
     def __init__(self, name="UNK", start_rank=0, wins=0, losses=0, draws=0):
@@ -18,7 +29,7 @@ class Footballer:
         #     self.victory_rate = 0
 
 
-available_players_pool = [
+players_pool = [
 Footballer(name="Bonfa", start_rank=2),
 Footballer(name="Karim", start_rank=3),
 Footballer(name="Mor", start_rank=3),
@@ -34,6 +45,22 @@ Footballer(name="Sboben", start_rank=5),
 Footballer(name="Moataz", start_rank=3),
 Footballer(name="Zebib", start_rank=2),
 ]
+available_players_pool = []
+# Footballer(name="Bonfa", start_rank=2),
+# Footballer(name="Karim", start_rank=3),
+# Footballer(name="Mor", start_rank=3),
+# Footballer(name="Akil", start_rank=3),
+# Footballer(name="Duccio", start_rank=1),
+# Footballer(name="Philipp", start_rank=3),
+# Footballer(name="Francesco collega", start_rank=3),
+# Footballer(name="Blallo", start_rank=2),
+# Footballer(name="Gio", start_rank=3),
+# Footballer(name="Pezzo", start_rank=3),
+# Footballer(name="Yarru", start_rank=3),
+# Footballer(name="Sboben", start_rank=5),
+# Footballer(name="Moataz", start_rank=3),
+# Footballer(name="Zebib", start_rank=2),
+# ]
 
 def teams_combinations(people, people_per_team) -> int:
     numerator = calc_factorial(people)
@@ -55,18 +82,63 @@ def make_teams(lst) -> tuple[list, list]:
             return team_a, team_b
     
 def print_teams_and_stats(team_a:list, team_b:list) -> str:
-    
-    # team_a_player = tuple[list, list]
-    # team_b_names = []
-    
     text_a, text_b = "", ""
     for pl in team_a:
-        # team_a_player.append(pl.name)
         text_a = text_a + f"{pl.name} ({pl.points})\n"
     for pl in team_b:
-        # team_b_names.append(pl.name)
         text_b = text_b + f"{pl.name} ({pl.points})\n"
-
     out_text = f"----------------------------------------\nTEAM A, overall score: {rank_of_list(team_a)}\n{text_a}----------------------------------------\nTEAM B, overall score: {rank_of_list(team_b)}\n{text_b}----------------------------------------\n"
+    return out_text
+
+def print_players_pool() -> str:
+    out_text=""
+    for pl in players_pool:
+        out_text = out_text + f"{pl.name} ({pl.points})\n"
+    print(out_text)
+    return out_text
+
+def is_player_new(new_pl:str) -> int:
+    for pl in players_pool:
+        if pl.name.lower() == new_pl.lower():
+            logger.info(f"{new_pl} not a new player.")
+            return 0
+    logger.info(f"{new_pl} new player!")
+    return 1
+
+def get_player_rank(new_pl:str) -> int:
+    for pl in players_pool:
+        if pl.name.lower() == new_pl.lower():
+            return pl.start_rank
+    return 3
+
+def insert_new_players_name(new_pl_name:str):
+    players_pool.append(Footballer(name=new_pl_name, start_rank=0))
+
+def insert_new_players_rank(new_pl_rank:int):
+    name_to_insert=get_last_inserted_name()
+    for pl in players_pool:
+        if pl.name == get_last_inserted_name():
+           players_pool.remove(pl)
+    players_pool.append(Footballer(name=name_to_insert, start_rank=new_pl_rank))
+
+def remove_uncomplete_player():
+    for pl in players_pool:
+        if pl.start_rank not in range (1, 6):
+            players_pool.remove(pl)
+
+def get_last_inserted_name() -> str:
+    return players_pool[len(players_pool) - 1].name
+
+def get_last_inserted_rank() -> int:
+    return players_pool[len(players_pool) - 1].start_rank
+
+## out_text = all championship stats
+def print_championship_stats() -> str:
+    if len(players_pool) == 0:
+        out_text = f"No players are registered"
+        return out_text
+    out_text = f"STATISTICS OF ALL {len(players_pool)} PLAYERS:\n"
+    for pl in players_pool:
+        out_text = out_text + f"{pl.name} ({pl.points})\n"
     return out_text
 
--- main.py
+++ main.py
@@ -21,11 +21,34 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-async def add_player(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def add_new_player(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text(
         "Insert player's name"
     )
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, input_text))
+    application.add_handler(handle_new_player_name_insertion)
+
+async def new_player_name_insertion(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    application.remove_handler(handle_new_player_name_insertion)
+    pl_name = update.message.text
+    if (is_player_new(pl_name)):
+        await update.message.reply_text(f"What is {pl_name}'s initial rank? (1 to 5)?")
+        insert_new_players_name(pl_name)
+        application.add_handler(handle_new_player_rank_insertion)
+    else:
+        await update.message.reply_text(f"{pl_name} is not a new player")
+
+async def new_player_rank_insertion(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    try:
+        pl_rank = int(update.message.text)
+    except:
+        pl_rank = 0
+    application.remove_handler(handle_new_player_rank_insertion)
+    if pl_rank in range(1, 6):
+        insert_new_players_rank(pl_rank)
+        await update.message.reply_text(f"{get_last_inserted_name()} enters Collebeato Championship with initial rank {get_last_inserted_rank()}!")
+    else:
+        remove_uncomplete_player()
+        await update.message.reply_text(f"Initial rank must be between 1 an 5")
 
 async def draw_teams(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     team_a, team_b = make_teams(available_players_pool)
@@ -35,17 +58,48 @@ async def draw_teams(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 async def new_match(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text("Insert players' names")
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, input_text))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, player_list_input))
 
-==== BASE ====
-async def input_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pl_name = update.message.text
-    await update.message.reply_text(f"What is {pl_name}'s initial rank? (1 to 5)?")
-    pl_rank = update.message.text
-    logger.info(f"Inserting {pl_name} with initial rank {pl_rank}")
-    players_pool.append(Footballer(name=pl_name, start_rank=pl_rank))
-    print(f"player:{players_pool[0].name}rank:{players_pool[0].start_rank}")
+async def player_list_input(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    players_names = update.message.text
+    players_names = players_names.split("\n")
+    out_text = f"You inserted {len(players_names)} players:\n{players_names}\n"
+    await update.message.reply_text(out_text)
+    # if is_player_new(pl_name):
+    #     ## Player not registered, using default initial rank (3)
+    #     players_pool.append(Footballer(name=pl_name, start_rank=3))
+    #     available_players_pool.append(Footballer(name=pl_name, start_rank=3))
+    #     out_text = f"Adding "
+    # else:
+    #     pl_rank = get_player_rank(pl_name)
+    #     available_players_pool.append(Footballer(name=pl_name, start_rank=pl_rank))
 
+## Printing to bot championship's stats
+async def get_players_pool(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"{print_championship_stats()}")
+
+async def clean_players_pool_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    players_pool.clear()
+    await update.message.reply_text(f"Players pool is now empty")
+#     reply_keyboard = [["Yes, proceed", "No, go back"]]
+#     await update.message.reply_text(
+#         "Are you sure you want to remove all the players?",
+#         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, input_field_placeholder="Yes or not?"),)
+#     application.add_handler(handle_clean_players_pool)
+
+# def perform_clean_players_pool(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     ans = update.message.text
+#     if (ans=="Yes, proceed"):
+#         players_pool.clear()
+#     else:
+#         pass
+#     reply_markup=ReplyKeyboardRemove(),
+
+
+## Some "interactive" handlers (i.e., gets removed/added depending on the execution flow)
+handle_new_player_name_insertion = MessageHandler(filters.TEXT & ~filters.COMMAND, new_player_name_insertion)
+handle_new_player_rank_insertion = MessageHandler(filters.TEXT & ~filters.COMMAND, new_player_rank_insertion)
+# handle_clean_players_pool = MessageHandler(filters.Regex("^(Yes, proceed|No, go back)$"), perform_clean_players_pool)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
@@ -53,7 +107,9 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
-    application.add_handler(CommandHandler('add_player', add_player))
+    application.add_handler(CommandHandler('add_new_player', add_new_player))
+    application.add_handler(CommandHandler('get_players_pool', get_players_pool))
+    application.add_handler(CommandHandler('clean_players_pool', clean_players_pool_request))
     application.add_handler(CommandHandler('draw_teams', draw_teams))
     application.add_handler(CommandHandler('new_match', new_match))
    
